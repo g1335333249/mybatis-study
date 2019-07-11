5389e2e55031c4a1783bec3cbac66bd2a988c286
关鹏 <guanpeng@hffss.com>
2019-07-11 09:03:14 +0800

    init

--- src/test/java/com/g1335333249/UserServiceTest.java
+++ src/test/java/com/g1335333249/UserServiceTest.java
@@ -1,15 +1,12 @@
 package com.g1335333249;
 
 import com.g1335333249.entity.User;
-import com.g1335333249.entity.UserExample;
 import com.g1335333249.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +17,7 @@ public class UserServiceTest {
 
     @Test
     public void contextLoads() {
-        List<User> users = userMapper.selectByExampleByAnnotation(1);
+        User users = userMapper.selectByPrimaryKey(1L);
         System.out.println(users);
     }
 
